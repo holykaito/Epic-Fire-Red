55893d1412c5d89ae8dd648dcfd2bf6151c88d70
holykaito <78580337+holykaito@users.noreply.github.com>
2026-08-23 23:12:18 +0700

    evo liên gen

--- src/party_menu.c
+++ src/party_menu.c
@@ -5376,11 +5376,11 @@ static void CB2_UseEvolutionStone(void)
 
 static bool8 MonCanEvolve(void)
 {
-    if (!IsNationalPokedexEnabled()
-     && GetEvolutionTargetSpecies(&gPlayerParty[gPartyMenu.slotId], EVO_MODE_ITEM_USE, gSpecialVar_ItemId) > KANTO_DEX_COUNT)
-        return FALSE;
-    else
-        return TRUE;
+    return GetEvolutionTargetSpecies(
+        &gPlayerParty[gPartyMenu.slotId],
+        EVO_MODE_ITEM_USE,
+        gSpecialVar_ItemId
+    ) != SPECIES_NONE;
 }
 
 u8 GetItemEffectType(u16 item)
