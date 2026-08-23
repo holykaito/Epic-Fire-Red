03b4424c5735bd0b8f54e26e39f8d676edf63f52
holykaito <78580337+holykaito@users.noreply.github.com>
2026-08-23 22:50:50 +0700

    Ghost&Dark type change

--- include/battle.h
+++ include/battle.h
@@ -472,8 +472,13 @@ extern struct BattleStruct *gBattleStruct;
         typeArg = gBattleMoves[move].type;                            \
 }
 
-#define IS_TYPE_PHYSICAL(moveType)(moveType < TYPE_MYSTERY)
-#define IS_TYPE_SPECIAL(moveType)(moveType > TYPE_MYSTERY)
+#define IS_TYPE_PHYSICAL(moveType) \
+    (((moveType) < TYPE_MYSTERY && (moveType) != TYPE_GHOST) \
+     || (moveType) == TYPE_DARK)
+
+#define IS_TYPE_SPECIAL(moveType) \
+    (((moveType) > TYPE_MYSTERY && (moveType) != TYPE_DARK) \
+     || (moveType) == TYPE_GHOST)
 
 #define TARGET_TURN_DAMAGED ((gSpecialStatuses[gBattlerTarget].physicalDmg != 0 || gSpecialStatuses[gBattlerTarget].specialDmg != 0))
 
